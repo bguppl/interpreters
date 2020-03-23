bf0db4ed165ea77393afd65639e675d05bb59477
Ben Eyal <bene@post.bgu.ac.il>
2020-03-23 10:04:52 +0200

    Add comment on the return value of the s-expression parser.

--- src/shared/s-expression.d.ts
+++ src/shared/s-expression.d.ts
@@ -1,5 +1,14 @@
 declare module 's-expression' {
     export type SexpString = String;
     export type StringTree = string | SexpString | StringTree[];
+
+    /*
+        The types returned by the parser are:
+        string - for any token which is not a string,
+                 according to the tokenization rules of S-expressions.
+        SexpString - for tokens of the form "..."
+        StringTree[] - for S-expressions that contain sub-expressions
+                       (of the form "(<expr1> ... <exprn>)")
+    */
     export default function parse(x: string): StringTree;
 }
