c3568491ef16c3a6b29ae82579de4f52a4745ff7
Ben Eyal <bene@post.bgu.ac.il>
2020-05-01 16:00:40 +0300

    Convert L7-eval.ts to Results

--- src/cps/L7-eval.ts
+++ src/cps/L7-eval.ts
@@ -1,138 +1,124 @@
 // L7-eval: CPS version of L5 with concrete data-structure continuations
 
-import { map, reduce, repeat, zipWith } from "ramda";
+import { map, repeat, zipWith } from "ramda";
 import { AppExp, CExp, DefineExp, Exp, IfExp, LetrecExp, LetExp,
-         Parsed, ProcExp, PrimOp, Program, SetExp, VarDecl } from '../L5/L5-ast';
+         ProcExp, Program, SetExp, VarDecl } from '../L5/L5-ast';
 import { isBoolExp, isLitExp, isNumExp, isPrimOp, isStrExp, isVarRef } from "../L5/L5-ast";
-import { parse, unparse } from "../L5/L5-ast";
-import { isBoolean, isEmpty, isNumber, isString } from "../L5/L5-ast";
-import { isAppExp, isDefineExp, isExp, isIfExp, isLetrecExp, isLetExp,
-         isProcExp, isProgram, isSetExp } from "../L5/L5-ast";
+import { parseL5Exp, unparse } from "../L5/L5-ast";
+import { isAppExp, isDefineExp, isIfExp, isLetrecExp, isLetExp,
+         isProcExp, isSetExp } from "../L5/L5-ast";
 import { applyEnv, applyEnvBdg, globalEnvAddBinding, makeExtEnv, setFBinding,
          theGlobalEnv, Env, ExtEnv } from "../L5/L5-env";
-import { isEmptySExp, isSymbolSExp, makeEmptySExp, parsedToString, EmptySExp } from '../L5/L5-value';
-import { isClosure, isCompoundSExp, makeClosure, makeCompoundSExp,
-         Closure, CompoundSExp, Value } from "../L5/L5-value";
-import { getErrorMessages, hasNoError, isError }  from "../shared/error";
-import { allT, first, rest } from '../shared/list';
+import { isClosure, makeClosure, Closure, Value, valueToString } from "../L5/L5-value";
+import { isEmpty, first, rest } from '../shared/list';
 import { applyPrimitive } from "../L5/evalPrimitive";
+import { Result, makeOk, makeFailure, bind, safe2, either } from "../shared/result";
+import { parse as p } from "../shared/parser";
 
 // ========================================================
 // Continuation datatype
-type Cont = (res: Value | Error) => Value | Error;
-type ContArray = (results: Array<Value | Error>) => Value | Error;
+type Cont = (res: Result<Value>) => Result<Value>;
+type ContArray = (results: Result<Value[]>) => Result<Value>;
 
-const applyCont = (cont: Cont, val: Value | Error) => cont(val);
-const applyContArray = (cont: ContArray, val: Array<Value | Error>) => cont(val);
+const applyCont = (cont: Cont, val: Result<Value>) => cont(val);
+const applyContArray = (cont: ContArray, val: Result<Value[]>) => cont(val);
 
 // ========================================================
 // Eval functions
 
-export const evalCont = (exp: CExp | Error, env: Env, cont: Cont): Value | Error =>
-    isError(exp)  ? applyCont(cont, exp) :
-    isNumExp(exp) ? applyCont(cont, exp.val) :
-    isBoolExp(exp) ? applyCont(cont, exp.val) :
-    isStrExp(exp) ? applyCont(cont, exp.val) :
-    isPrimOp(exp) ? applyCont(cont, exp) :
+export const evalCont = (exp: CExp, env: Env, cont: Cont): Result<Value> =>
+    isNumExp(exp) ? applyCont(cont, makeOk(exp.val)) :
+    isBoolExp(exp) ? applyCont(cont, makeOk(exp.val)) :
+    isStrExp(exp) ? applyCont(cont, makeOk(exp.val)) :
+    isPrimOp(exp) ? applyCont(cont, makeOk(exp)) :
     isVarRef(exp) ? applyCont(cont, applyEnv(env, exp.var)) :
-    isLitExp(exp) ? applyCont(cont, exp.val) :
+    isLitExp(exp) ? applyCont(cont, makeOk(exp.val)) :
     isIfExp(exp) ? evalIf(exp, env, cont) :
     isProcExp(exp) ? evalProc(exp, env, cont) :
     isLetExp(exp) ? evalLet(exp, env, cont) :
     isLetrecExp(exp) ? evalLetrec(exp, env, cont) :
     isSetExp(exp) ? evalSet(exp, env, cont) :
     isAppExp(exp) ? evalApp(exp, env, cont) :
-    applyCont(cont, Error(`Bad L5 AST ${exp}`));
+    applyCont(cont, makeFailure(`Bad L5 AST ${exp}`));
 
-export const isTrueValue = (x: Value | Error): boolean | Error =>
-    isError(x) ? x :
+export const isTrueValue = (x: Value): boolean =>
     ! (x === false);
 
-const evalIf = (exp: IfExp, env: Env, cont: Cont): Value | Error =>
+const evalIf = (exp: IfExp, env: Env, cont: Cont): Result<Value> =>
     evalCont(exp.test, env, makeIfCont(exp, env, cont));
 
 const makeIfCont = (exp: IfExp, env: Env, cont: Cont) =>
-    (testVal: Value | Error) =>
-        isError(testVal) ? applyCont(cont, testVal) :
-        isTrueValue(testVal) ? evalCont(exp.then, env, cont) :
-        evalCont(exp.alt, env, cont);
+    (testVal: Result<Value>) =>
+        bind(testVal,
+             test => isTrueValue(test) ? evalCont(exp.then, env, cont) : evalCont(exp.alt, env, cont));
 
 
-const evalProc = (exp: ProcExp, env: Env, cont: Cont): Value | Error =>
-    applyCont(cont, makeClosure(exp.args, exp.body, env));
+const evalProc = (exp: ProcExp, env: Env, cont: Cont): Result<Value> =>
+    applyCont(cont, makeOk(makeClosure(exp.args, exp.body, env)));
 
 // Return the vals (rhs) of the bindings of a let expression
 const letVals = (exp: LetExp | LetrecExp): CExp[] =>
-        map((b) => b.val, exp.bindings);
+    map((b) => b.val, exp.bindings);
 
 // Return the vars (lhs) of the bindings of a let expression
 const letVars = (exp: LetExp | LetrecExp): string[] =>
-        map((b) => b.var.var, exp.bindings);
+    map((b) => b.var.var, exp.bindings);
 
 // LET: Direct evaluation rule without syntax expansion
 // compute the values, extend the env, eval the body.
-const evalLet = (exp: LetExp, env: Env, cont: Cont): Value | Error =>
+const evalLet = (exp: LetExp, env: Env, cont: Cont): Result<Value> =>
     evalExps(letVals(exp), env, makeLetCont(exp, env, cont));
 
 const makeLetCont = (exp: LetExp, env: Env, cont: Cont) =>
-    (vals: Array<Value | Error>) =>
-        hasNoError(vals) ? evalSequence(exp.body, makeExtEnv(letVars(exp), vals, env), cont) :
-        applyCont(cont, Error(getErrorMessages(vals)));
+    (vals: Result<Value[]>) =>
+        bind(vals,
+             (values: Value[]) => evalSequence(exp.body, makeExtEnv(letVars(exp), values, env), cont));
 
 // Evaluate an array of expressions in sequence - pass the result of the last element to cont
 // @Pre: exps is not empty
-export const evalSequence = (exps: Exp[], env: Env, cont: Cont): Value | Error =>
-    isEmpty(exps) ? applyCont(cont, Error("Empty Sequence")) :
+export const evalSequence = (exps: Exp[], env: Env, cont: Cont): Result<Value> =>
+    isEmpty(exps) ? applyCont(cont, makeFailure("Empty Sequence")) :
     evalSequenceFR(first(exps), rest(exps), env, cont);
 
-const evalSequenceFR = (exp: Exp, exps: Exp[], env: Env, cont: Cont): Value | Error =>
+const evalSequenceFR = (exp: Exp, exps: Exp[], env: Env, cont: Cont): Result<Value> =>
     isDefineExp(exp) ? evalDefineExps(exp, exps, cont) :
     isEmpty(exps) ? evalCont(exp, env, cont) :
     evalCont(exp, env, makeFirstCont(exps, env, cont));
 
 export const makeFirstCont = (exps: Exp[], env: Env, cont: Cont) =>
-    (firstVal: Value | Error) =>
-        isError(firstVal) ? applyCont(cont, firstVal) :
-        evalSequence(exps, env, cont);
+    (firstVal: Result<Value>) => bind(firstVal, _ => evalSequence(exps, env, cont));
 
 // define always updates theGlobalEnv
 // We only expect defineExps at the top level.
-export const evalDefineExps = (exp: DefineExp, exps: Exp[], cont: Cont): Value | Error =>
+export const evalDefineExps = (exp: DefineExp, exps: Exp[], cont: Cont): Result<Value> =>
     evalCont(exp.val, theGlobalEnv, makeDefCont(exp, exps, cont));
 
 export const makeDefCont = (exp: DefineExp, exps: Exp[], cont: Cont) =>
-    (rhsVal: Value | Error) => {
-        if (isError(rhsVal))
-            return applyCont(cont, rhsVal);
-        else {
-            globalEnvAddBinding(exp.var.var, rhsVal);
-            return evalSequence(exps, theGlobalEnv, cont);
-        }
-    };
+    (rhsVal: Result<Value>) =>
+        bind(rhsVal, rhs => { globalEnvAddBinding(exp.var.var, rhs);
+                              return evalSequence(exps, theGlobalEnv, cont); });
 
 // Evaluate an array of expressions - pass the result as an array to the continuation
-export const evalExps = (exps: Exp[], env: Env, cont: ContArray): Value | Error =>
-    isEmpty(exps) ? applyContArray(cont, []) :
+export const evalExps = (exps: Exp[], env: Env, cont: ContArray): Result<Value> =>
+    isEmpty(exps) ? applyContArray(cont, makeOk([])) :
     evalExpsFR(first(exps), rest(exps), env, cont)
 
-const evalExpsFR = (exp: Exp, exps: Exp[], env: Env, cont: ContArray): Value | Error =>
-    isDefineExp(exp) ? applyContArray(cont, [Error("Unexpected define: "+unparse(exp))]) :
+const evalExpsFR = (exp: Exp, exps: Exp[], env: Env, cont: ContArray): Result<Value> =>
+    isDefineExp(exp) ? applyContArray(cont, bind(unparse(exp), e => makeFailure(`Unexpected define: ${e}`))) :
     evalCont(exp, env, makeExpsCont1(exps, env, cont));
 
-const makeExpsCont1 = (exps: Exp[], env: Env, cont: ContArray) =>
-    (firstVal: Value | Error) => 
-        isError(firstVal) ? applyContArray(cont, [firstVal]) :    
-        evalExps(exps, env, makeExpsCont2(firstVal, cont));
+const makeExpsCont1 = (exps: Exp[], env: Env, cont: ContArray): Cont =>
+    (firstVal: Result<Value>) => bind(firstVal, first => evalExps(exps, env, makeExpsCont2(first, cont)));
 
-const makeExpsCont2 = (firstVal: Value, cont: ContArray) =>
-    (restVals: Array<Value | Error>) => applyContArray(cont, [firstVal, ...restVals]);
+const makeExpsCont2 = (firstVal: Value, cont: ContArray): ContArray =>
+    (restVals: Result<Value[]>) => bind(restVals, rest => applyContArray(cont, makeOk([firstVal, ...rest])));
 
 // LETREC: Direct evaluation rule without syntax expansion
 // 1. extend the env with vars initialized to void (temporary value)
 // 2. compute the vals in the new extended env
 // 3. update the bindings of the vars to the computed vals
 // 4. compute body in extended env
-export const evalLetrec = (exp: LetrecExp, env: Env, cont: Cont): Value | Error => {
+export const evalLetrec = (exp: LetrecExp, env: Env, cont: Cont): Result<Value> => {
     const vars = letVars(exp);
     const vals = letVals(exp);
     const extEnv = makeExtEnv(vars, repeat(undefined, vars.length), env);
@@ -141,69 +127,47 @@ export const evalLetrec = (exp: LetrecExp, env: Env, cont: Cont): Value | Error 
 }
 
 export const makeLetrecCont = (exp: LetrecExp, extEnv: ExtEnv, cont: Cont): ContArray =>
-    (cvals: Array<Value | Error>) => {
-        if (hasNoError(cvals)) {
-            // Bind vars in extEnv to the new values
-            zipWith((bdg, cval) => setFBinding(bdg, cval), extEnv.frame.fbindings, cvals);
-            return evalSequence(exp.body, extEnv, cont);
-        } else {
-            return applyCont(cont, Error(getErrorMessages(cvals)));
-        }
-    };
-
+    (cvals: Result<Value[]>) =>
+        bind(cvals, vals => { zipWith((bdg, val) => setFBinding(bdg, val), extEnv.frame.fbindings, vals);
+                              return evalSequence(exp.body, extEnv, cont); });
 
 // L4-eval-box: Handling of mutation with set!
-export const evalSet = (exp: SetExp, env: Env, cont: Cont): Value | Error =>
+export const evalSet = (exp: SetExp, env: Env, cont: Cont): Result<Value> =>
     evalCont(exp.val, env, makeSetCont(exp, env, cont));
 
 export const makeSetCont = (exp: SetExp, env: Env, cont: Cont) =>
-    (rhsVal: Value | Error) => {
-        if (isError(rhsVal))
-            return applyCont(cont, rhsVal);
-        else {
-            const v = exp.var.var;
-            const bdg = applyEnvBdg(env, v);
-            if (isError(bdg)) {
-                return applyCont(cont, Error(`Var not found ${v}`));
-            } else {
-                setFBinding(bdg, rhsVal);
-                return applyCont(cont, undefined);
-            }
-        }
-    };
+    (rhsVal: Result<Value>) =>
+        bind(rhsVal,
+             rhs => bind(applyEnvBdg(env, exp.var.var),
+                         bdg => { setFBinding(bdg, rhs);
+                                  return applyCont(cont, makeOk(undefined))}));
 
-const evalApp = (exp: AppExp, env: Env, cont: Cont): Value | Error =>
+const evalApp = (exp: AppExp, env: Env, cont: Cont): Result<Value> =>
     evalCont(exp.rator, env,
         (proc) => evalExps(exp.rands, env,
                     (args) => applyProcedure(proc, args, cont)));
 
-const applyProcedure = (proc: Value | Error, args: Array<Value | Error>, cont: Cont): Value | Error =>
-    isError(proc) ? applyCont(cont, proc) :
-    !hasNoError(args) ? applyCont(cont, Error(`Bad argument: ${getErrorMessages(args)}`)) :
-    isPrimOp(proc) ? applyCont(cont, applyPrimitive(proc, args)) :
-    isClosure(proc) ? applyClosure(proc, args, cont) :
-    applyCont(cont, Error(`Bad procedure ${JSON.stringify(proc)}`));
+const applyProcedure = (proc: Result<Value>, args: Result<Value[]>, cont: Cont): Result<Value> =>
+    safe2((proc: Value, args: Value[]) => isPrimOp(proc) ? applyCont(cont, applyPrimitive(proc, args)) :
+                                          isClosure(proc) ? applyClosure(proc, args, cont) :
+                                          applyCont(cont, makeFailure(`Bad procedure: ${JSON.stringify(proc)}`)))
+        (proc, args);
 
-const applyClosure = (proc: Closure, args: Value[], cont: Cont): Value | Error => {
+const applyClosure = (proc: Closure, args: Value[], cont: Cont): Result<Value> => {
     let vars = map((v: VarDecl) => v.var, proc.params);
     return evalSequence(proc.body, makeExtEnv(vars, args, proc.env), cont);
 }
 
 // Final continuation
-export const topCont: Cont = (val) => { console.log(parsedToString(val)); return val; }
+export const topCont: Cont = (val) => {
+    either(val, v => console.log(valueToString(v)), console.error);
+    return val;
+}
 
 // Evaluate a program
 // Main program
-export const evalProgram = (program: Program): Value | Error =>
+export const evalProgram = (program: Program): Result<Value> =>
     evalSequence(program.exps, theGlobalEnv, topCont);
 
-export const evalParse = (s: string): Value | Error => {
-    let ast: Parsed | Error = parse(s);
-    if (isProgram(ast)) {
-        return evalProgram(ast);
-    } else if (isExp(ast)) {
-        return evalSequence([ast], theGlobalEnv, topCont);
-    } else {
-        return ast;
-    }
-}
+export const evalParse = (s: string): Result<Value> =>
+    bind(bind(p(s), parseL5Exp), exp => evalSequence([exp], theGlobalEnv, topCont));
